82d8f6db15930f4be2fa598c92d86131b81ef8e3
Kenta Sato <bicycle1885@gmail.com>
2018-08-13 03:36:11 +0900

    update scripts

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,31 +1,37 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
+
+name = "XzBuilder"
+version = v"5.2.4"
 
 # Collection of sources required to build XzBuilder
 sources = [
-    "https://tukaani.org/xz/xz-5.2.3.tar.xz" =>
-    "7876096b053ad598c31f6df35f7de5cd9ff2ba3162e5a5554e4fc198447e0347",
+    "https://tukaani.org/xz/xz-$(version).tar.xz" =>
+    "9717ae363760dedf573dad241420c5fea86256b65bc21d2cf71b2b12f0544f4b",
+    "https://tukaani.org/xz/xz-$(version)-windows.zip" =>
+    "9a5163623f435b6fa0844b6b884babd6bf4f8d876ae2d8134deeb296afd49c61",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/xz-5.2.3/
-./configure --prefix=$prefix --host=$target
-make
-make install
+if [ ${target} = "x86_64-w64-mingw32" ]; then
+    mkdir -p ${WORKSPACE}/destdir/bin/
+    cp bin_x86-64/liblzma.dll ${WORKSPACE}/destdir/bin/
+elif [ ${target} = "i686-w64-mingw32" ]; then
+    mkdir -p ${WORKSPACE}/destdir/bin/
+    cp bin_i686/liblzma.dll ${WORKSPACE}/destdir/bin/
+else
+    cd $WORKSPACE/srcdir/xz-*
+    ./configure --prefix=${prefix} --host=${target}
+    make
+    make install
+fi
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
-    BinaryProvider.Linux(:x86_64, :glibc),
-    BinaryProvider.Linux(:aarch64, :glibc),
-    BinaryProvider.Linux(:armv7l, :glibc),
-    BinaryProvider.Linux(:powerpc64le, :glibc),
-    BinaryProvider.MacOS(),
-    BinaryProvider.Windows(:i686),
-    BinaryProvider.Windows(:x86_64),
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -36,4 +42,4 @@ products(prefix) = [
 dependencies = []
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "XzBuilder", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
